1b73b860c0199e73a9b94027e7e47db623e2f6d1
Jovy <zjjovyy8215@gmail.com>
2024-11-04 09:45:01 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 'use client';
 import BlueMatrix from "./components/blue-matrix";
-import ItemList from "@/app/item-list";
+// import ItemList from "./components/item-list";
 import { ChartType, VisualizationTask, VisualizationContext } from '@/app/types';
 
 export default function Home() {
 
   return (
     <div className="min-h-screen p-8 sm:p-20">
-      <ItemList items={items} />
       <BlueMatrix />
     </div>
   );
