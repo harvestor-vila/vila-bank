0d8ebf52a38faf87f1be3e746143ac6c607614a0
Harry Guan <guanhuai2014@gmail.com>
2024-11-18 16:49:46 -0600

    deployment ready

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true, // Required for static exports
+    },
+  };
+  
+  module.exports = nextConfig;
+  
