df076d932ea5a5f29192d4854c6acd389a526d11
Harry Guan <guanhuai2014@gmail.com>
2024-11-17 21:42:19 -0600

    temporary reversion

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/vila-bank', // Replace with your GitHub repository name
-  assetPrefix: '/vila-bank/', // Same as above
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
